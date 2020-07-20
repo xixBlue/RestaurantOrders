9157136ff019608f294a348fcb1d3a1176aeea8b
AUJuan Villaman <AUJ.Villaman@autolus.com>
2020-07-20 19:55:43 -0400

    Co-authored-by: eddygutierrez175@gmail.com <eddygutierrez175@gmail.com> Co-authored-by: danego <nevesdane@gmail.com> Co-authored-by: jasineath@outlook.com <jasineath@outlook.com> Co-authored-by: csaisem <csaisem@yahoo.com>

--- Pages/new-order-app.js
+++ Pages/new-order-app.js
@@ -55,8 +55,11 @@ const drinkFour = document.getElementById('drink-four');
 const drinkFive = document.getElementById('drink-five');
 
 //Declaring Submit/Complete Order Buttons
+const customerName = document.getElementById('customer-name');
 const submit = document.getElementById('submit');
 const completeOrder = document.getElementById('complete-order');
+const deleteBtn = document.getElementById('delete');
+let deleteEnabled = false;
 
 /*
     1. Create an order counter
@@ -66,8 +69,13 @@ const completeOrder = document.getElementById('complete-order');
         Server
         Table
     4. Submit button would push to Array and populate Order Created
-        Name/Order Object [{
-            name: name
+        OrderItems = {
+            appetizer:{ 
+                {app1: amount,
+                app2: amount}
+            }
+        }
+        Order items Object [
             appetizers: {      //add properties to this object as they are ordered, or increment quantity
                 app1: quantity
             }
@@ -93,36 +101,14 @@ const orderObject = {
     orderNumber: orderCounter,
     server: 0,
     table: 0,
-    orderItems: []
+    customerName: '',
+    orderItems: {
+        appetizers: {},
+        entrees: {},
+        desserts: {},
+        drinks: {}
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //Server button styling to keep pressed and display a toggle dynamic
 const clearServerToggles = () => {
@@ -251,9 +237,309 @@ tableFifteen.onclick = function(e){
     orderObject.table = 15;
 } 
 
+//Adding name to orderItems array inside 
+customerName.onchange = function() {
+    orderObject.customerName = customerName.value;
+}
+
+//OrderItems EventHandlers
+appOne.onclick = function(e) {
+    //check if appetizer is already in order
+    //if yes, increase amount
+    if (!deleteEnabled){
+        if (orderObject.orderItems.appetizers.appOne) {
+            orderObject.orderItems.appetizers.appOne++;
+        } else {
+            orderObject.orderItems.appetizers.appOne = 1;
+            this.style.borderStyle = 'inset';
+        }
+    } else {
+        if (orderObject.orderItems.appetizers.appOne > 1) {
+            orderObject.orderItems.appetizers.appOne--;
+        } else {
+            delete orderObject.orderItems.appetizers.appOne;
+            //orderObject.orderItems.appetizers.appOne = 0;
+            this.style.borderStyle = 'none';
+        }
+    }   
+}
+appTwo.onclick = function(e) {
+    //check if appetizer is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.appetizers.appTwo) {
+        orderObject.orderItems.appetizers.appTwo++;
+    } else {
+        orderObject.orderItems.appetizers.appTwo = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+appThree.onclick = function(e) {
+    //check if appetizer is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.appetizers.appThree) {
+        orderObject.orderItems.appetizers.appThree++;
+    } else {
+        orderObject.orderItems.appetizers.appThree = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+appFour.onclick = function(e) {
+    //check if appetizer is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.appetizers.appFour) {
+        orderObject.orderItems.appetizers.appFour++;
+    } else {
+        orderObject.orderItems.appetizers.appFour = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+appFive.onclick = function(e) {
+    //check if appetizer is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.appetizers.appFive) {
+        orderObject.orderItems.appetizers.appFive++;
+    } else {
+        orderObject.orderItems.appetizers.appFive = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeOne.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeOne) {
+        orderObject.orderItems.entrees.entreeOne++;
+    } else {
+        orderObject.orderItems.entrees.entreeOne = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeTwo.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeTwo) {
+        orderObject.orderItems.entrees.entreeTwo++;
+    } else {
+        orderObject.orderItems.entrees.entreeTwo = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeThree.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeThree) {
+        orderObject.orderItems.entrees.entreeThree++;
+    } else {
+        orderObject.orderItems.entrees.entreeThree = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeFour.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeFour) {
+        orderObject.orderItems.entrees.entreeFour++;
+    } else {
+        orderObject.orderItems.entrees.entreeFour = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeFive.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeFive) {
+        orderObject.orderItems.entrees.entreeFive++;
+    } else {
+        orderObject.orderItems.entrees.entreeFive = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeSix.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeSix) {
+        orderObject.orderItems.entrees.entreeSix++;
+    } else {
+        orderObject.orderItems.entrees.entreeSix = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeSeven.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeSeven) {
+        orderObject.orderItems.entrees.entreeSeven++;
+    } else {
+        orderObject.orderItems.entrees.entreeSeven = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeEight.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeEight) {
+        orderObject.orderItems.entrees.entreeEight++;
+    } else {
+        orderObject.orderItems.entrees.entreeEight = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeNine.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeNine) {
+        orderObject.orderItems.entrees.entreeNine++;
+    } else {
+        orderObject.orderItems.entrees.entreeNine = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+entreeTen.onclick = function(e) {
+    //check if Entree is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.entrees.entreeTen) {
+        orderObject.orderItems.entrees.entreeTen++;
+    } else {
+        orderObject.orderItems.entrees.entreeTen = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+dessertOne.onclick = function(e) {
+    //check if Dessert is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.desserts.dessertOne) {
+        orderObject.orderItems.desserts.dessertOne++;
+    } else {
+        orderObject.orderItems.desserts.dessertOne = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+dessertTwo.onclick = function(e) {
+    //check if Dessert is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.desserts.dessertTwo) {
+        orderObject.orderItems.desserts.dessertTwo++;
+    } else {
+        orderObject.orderItems.desserts.dessertTwo = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+dessertThree.onclick = function(e) {
+    //check if Dessert is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.desserts.dessertThree) {
+        orderObject.orderItems.desserts.dessertThree++;
+    } else {
+        orderObject.orderItems.desserts.dessertThree = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+dessertFour.onclick = function(e) {
+    //check if Dessert is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.desserts.dessertFour) {
+        orderObject.orderItems.desserts.dessertFour++;
+    } else {
+        orderObject.orderItems.desserts.dessertFour = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+dessertFive.onclick = function(e) {
+    //check if Dessert is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.desserts.dessertFive) {
+        orderObject.orderItems.desserts.dessertFive++;
+    } else {
+        orderObject.orderItems.desserts.dessertFive = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+drinkOne.onclick = function(e) {
+    //check if Drink is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.drinks.drinkOne) {
+        orderObject.orderItems.drinks.drinkOne++;
+    } else {
+        orderObject.orderItems.drinks.drinkOne = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+drinkTwo.onclick = function(e) {
+    //check if Drink is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.drinks.drinkTwo) {
+        orderObject.orderItems.drinks.drinkTwo++;
+    } else {
+        orderObject.orderItems.drinks.drinkTwo = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+drinkThree.onclick = function(e) {
+    //check if Drink is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.drinks.drinkThree) {
+        orderObject.orderItems.drinks.drinkThree++;
+    } else {
+        orderObject.orderItems.drinks.drinkThree = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+drinkFour.onclick = function(e) {
+    //check if Drink is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.drinks.drinkFour) {
+        orderObject.orderItems.drinks.drinkFour++;
+    } else {
+        orderObject.orderItems.drinks.drinkFour = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
+drinkFive.onclick = function(e) {
+    //check if Drink is already in order
+    //if yes, increase amount
+    if (orderObject.orderItems.drinks.drinkFive) {
+        orderObject.orderItems.drinks.drinkFive++;
+    } else {
+        orderObject.orderItems.drinks.drinkFive = 1;
+        this.style.borderStyle = 'inset';
+    }
+}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+deleteBtn.onclick = function (e) {
+    this.style.borderStyle = (this.style.borderStyle !== 'inset' ? 'inset' : 'outset');
+    this.style.backgroundColor = (this.style.backgroundColor === 'red' ? 'transparent' : 'red');
+    if (deleteEnabled) {
+        deleteEnabled = false;
+        this.textContent = 'Delete';
+    }
+    else {
+        deleteEnabled = 'true';
+        this.textContent = 'Delete Enabled!';
+    }
+}
 
 
 
@@ -264,6 +550,16 @@ tableFifteen.onclick = function(e){
 //CompleteOrder Button Function
 completeOrder.onclick = function(e){
     console.log(orderObject);
+    //add new html elements for each objectItem in objectOrder
+    // const displayOrderSection = document.getElementById('display-order');
+    // for (let prop in orderObject.orderItems) {
+    //     for (let innerProp in prop) {
+    //         const pElement = document.createElement('p');
+    //         pElement.innerText = `${innerProp}: ${innerProp.value}`;
+    //         displayOrderSection.appendChild(pElement);
+    //     }
+    // }
+
     orderCounter++;
     orderObject.orderNumber = orderCounter;
 }
---
+++ task.md
@@ -0,0 +1,32 @@
+# GitFitCode Pair Coding Challenge!
+
+### Let's write some code to order some awesome food!
+
+The **main objective** is to allow a awesome restraunt owner handle their orders! The user should be able to add orders, which is a customer name, table number, and food item. The owner should also be able to visually see all existing orders, edit, and delete them. Please place this information in a layout such as a table so its presented well. Also we really want to make the employees life easier so we want to describe each table by sharing who ordered what in a sentence format. This time we haven't added the HTML, JS, or CSS files. We would like y'all to do this yourself :)  **PLEASE** do not look for solutions online! This is only hurting yourself and the rest of the team. Please use the README.md file to describe the code so other developers know how everything works and maybe some points on future goals.
+
+*Bonus Points:*
+
+Style the game to make it more appealing! After you get done with the core features, maybe you can write out some desired features in the README.md. MAKE IT SEXY!
+
+**Due Date:** 7/23/2020
+
+Please submit a zip file to the check ins channel or send a github link!
+
+##### Here are some pointers:
+Here some data to help get y'all started!
+
+Input: orders = [["Courtney","12","Fried Chicken"],["Juan","12","Fried Chicken"],["Eddie","12","PB&Js"],["Justin","1","Canadian Waffles"],["Sirrele","1","Canadian Waffles"]]
+Output: [["Table","Canadian Waffles","Fried Chicken", "PB&Js"],["1","2","0", "0", "0"],["12","0","2","1"]]
+Explanation:
+For the table 1: Justin and Sirrele ordered "Canadian Waffles".
+For the table 12: Courtney, Juan orded fried chicken and Eddie ordered "PP&Js".
+
+* Support each other! Make sure that y'all really breakdown these problems into managable parts that you can solve. It is a lot easier to solve a bunch of tiny problems vs a large one.
+
+* Make sure to use proper styling and to comment your code!
+
+* Remember this is a team effort! Encourage each other, and really make sure to support each other.
+
+* Before sovling any problem, really breakdown all the rules. Maybe even play a couple games so you can easily describe it.
+
+* Feel free to contact @GitFitDev or @Sirrele for assitance! I hope y'all have fun doing this together!
